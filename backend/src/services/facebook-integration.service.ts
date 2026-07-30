7f0b53af6106eb44b92a20b7dc87def305672d2d
Charanjeetsingh068 <charanjeetsingh068@gmail.com>
2026-07-30 17:43:25 +0530

    changes

--- backend/src/services/facebook-integration.service.ts
+++ backend/src/services/facebook-integration.service.ts
@@ -209,7 +209,7 @@ export class FacebookIntegrationService {
       }
     }
     const redirectUri = encodeURIComponent(`${baseUrl}/integrations/facebook/callback`);
-    const scopes = encodeURIComponent('public_profile,email,pages_show_list');
+    const scopes = encodeURIComponent('public_profile,email,pages_show_list,pages_read_engagement,leads_retrieval,business_management');
     
     const oauthUrl = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=code`;
 
